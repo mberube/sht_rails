5744cd2292f784ff11fdce32ba9fe1a928dc8d09
Mathieu Berube <mathieu.berube@gmail.com>
2016-01-26 04:03:54 -0500

    Fix for less-rails 4.7.1 and rails 4.2.5.1

--- lib/sht_rails/engine.rb
+++ lib/sht_rails/engine.rb
@@ -3,10 +3,10 @@ module ShtRails
     config.before_configuration do |app|
       app.paths['app/views'] << ShtRails.template_base_path
     end
-    
+
     initializer "sprockets.sht_rails", :after => "sprockets.environment", :group => :all do |app|
-      next unless app.assets
-      app.assets.register_engine(".#{ShtRails.template_extension}", Tilt)
+      next unless Sprockets
+      Sprockets.register_engine(".#{ShtRails.template_extension}", Tilt)
       app.config.assets.paths << ShtRails.template_base_path
     end
   end
